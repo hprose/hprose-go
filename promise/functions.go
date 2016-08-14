3ce2696dd0cb1e94fc10f2fecc6e92d880e1eab1
Ma Bingyao <mabingyao@gmail.com>
2016-08-14 11:34:01 +0800

    Added Each & Every

--- promise/functions.go
+++ promise/functions.go
@@ -12,7 +12,7 @@
  *                                                        *
  * some functions of promise for Go.                      *
  *                                                        *
- * LastModified: Aug 13, 2016                             *
+ * LastModified: Aug 14, 2016                             *
  * Author: Ma Bingyao <andot@hprose.com>                  *
  *                                                        *
 \**********************************************************/
@@ -29,10 +29,10 @@ import (
 // passed promise that rejects.
 func All(iterable ...interface{}) Promise {
 	count := int64(len(iterable))
-	result := make([]interface{}, count)
 	if count == 0 {
-		return Resolve(result)
+		return Resolve(nil)
 	}
+	result := make([]interface{}, count)
 	promise := New()
 	for index, value := range iterable {
 		ToPromise(value).Then(func(index int) func(value interface{}) {
@@ -86,4 +86,57 @@ func Any(iterable ...interface{}) Promise {
 		})
 	}
 	return promise
+}
+
+// Each function executes a provided function once per element of iterable.
+//
+// If any of the promises in iterable is rejected, the callback will not be
+// executed. the returned promise will be rejected with the rejection reason
+// of the first promise that was rejected.
+//
+// Parameters:
+//     callback
+//         Function to execute for each element, taking two arguments:
+//         index
+//             The index of the current element being processed.
+//         value
+//             The current element being processed.
+func Each(callback func(int, interface{}), iterable ...interface{}) Promise {
+	return All(iterable...).Then(func(a interface{}) {
+		if a == nil {
+			return
+		}
+		iterable := a.([]interface{})
+		for index, value := range iterable {
+			callback(index, value)
+		}
+	})
+}
+
+// Every function tests whether all elements in the iterable pass the test implemented by the provided function.
+//
+// If any of the promises in iterable is rejected, the callback will not be
+// executed. the returned promise will be rejected with the rejection reason
+// of the first promise that was rejected.
+//
+// Parameters:
+//     callback
+//         Function to test for each element, taking two arguments:
+//         index
+//             The index of the current element being processed.
+//         value
+//             The current element being processed.
+func Every(callback func(int, interface{}) bool, iterable ...interface{}) Promise {
+	return All(iterable...).Then(func(a interface{}) (interface{}, error) {
+		if a == nil {
+			return true, nil
+		}
+		iterable := a.([]interface{})
+		for index, value := range iterable {
+			if !callback(index, value) {
+				return false, nil
+			}
+		}
+		return true, nil
+	})
 }
