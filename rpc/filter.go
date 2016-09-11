9b3734ba9f9c6849af79d11329a31f5727e17551
Ma Bingyao <mabingyao@gmail.com>
2016-09-11 14:32:26 +0800

    Added hprose Filter

---
+++ rpc/filter.go
@@ -0,0 +1,26 @@
+/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * rpc/filter.go                                          *
+ *                                                        *
+ * hprose filter interface for Go.                        *
+ *                                                        *
+ * LastModified: Sep 11, 2016                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+package rpc
+
+// Filter is hprose filter
+type Filter interface {
+	InputFilter(data []byte, context Context) []byte
+	OutputFilter(data []byte, context Context) []byte
+}
