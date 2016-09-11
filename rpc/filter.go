2f44278b93dd014c33dd91d549897be585700dbf
Ma Bingyao <mabingyao@gmail.com>
2016-09-11 19:58:13 +0800

    Update filter.go

--- rpc/filter.go
+++ rpc/filter.go
@@ -25,13 +25,13 @@ type Filter interface {
 	OutputFilter(data []byte, context Context) []byte
 }
 
-// FilterManager is the filter manager
-type FilterManager struct {
+// filterManager is the filter manager
+type filterManager struct {
 	filters []Filter
 }
 
 // Filter return the first filter
-func (fm *FilterManager) Filter() Filter {
+func (fm *filterManager) Filter() Filter {
 	if len(fm.filters) == 0 {
 		return nil
 	}
@@ -39,7 +39,7 @@ func (fm *FilterManager) Filter() Filter {
 }
 
 // FilterByIndex return the filter by index
-func (fm *FilterManager) FilterByIndex(index int) Filter {
+func (fm *filterManager) FilterByIndex(index int) Filter {
 	n := len(fm.filters)
 	if index < 0 && index >= n {
 		return nil
@@ -48,20 +48,20 @@ func (fm *FilterManager) FilterByIndex(index int) Filter {
 }
 
 // SetFilter will replace the current filter settings
-func (fm *FilterManager) SetFilter(filter ...Filter) {
+func (fm *filterManager) SetFilter(filter ...Filter) {
 	fm.filters = make([]Filter, len(filter))
 	fm.AddFilter(filter...)
 }
 
 // AddFilter add the filter to this FilterManager
-func (fm *FilterManager) AddFilter(filter ...Filter) {
+func (fm *filterManager) AddFilter(filter ...Filter) {
 	if len(filter) > 0 {
 		fm.filters = append(fm.filters, filter...)
 	}
 }
 
 // RemoveFilterByIndex remove the filter by the index
-func (fm *FilterManager) RemoveFilterByIndex(index int) {
+func (fm *filterManager) RemoveFilterByIndex(index int) {
 	n := len(fm.filters)
 	if index < 0 && index >= n {
 		return
@@ -73,7 +73,7 @@ func (fm *FilterManager) RemoveFilterByIndex(index int) {
 	}
 }
 
-func (fm *FilterManager) removeFilter(filter Filter) {
+func (fm *filterManager) removeFilter(filter Filter) {
 	n := len(fm.filters)
 	for i := 0; i < n; i++ {
 		if fm.filters[i] == filter {
@@ -84,21 +84,21 @@ func (fm *FilterManager) removeFilter(filter Filter) {
 }
 
 // RemoveFilter remove the filter from this FilterManager
-func (fm *FilterManager) RemoveFilter(filter ...Filter) {
+func (fm *filterManager) RemoveFilter(filter ...Filter) {
 	n := len(filter)
 	for i := 0; i < n; i++ {
 		fm.removeFilter(filter[i])
 	}
 }
 
-func (fm *FilterManager) inputFilter(data []byte, context Context) []byte {
+func (fm *filterManager) inputFilter(data []byte, context Context) []byte {
 	for i := len(fm.filters) - 1; i >= 0; i-- {
 		data = fm.filters[i].InputFilter(data, context)
 	}
 	return data
 }
 
-func (fm *FilterManager) outputFilter(data []byte, context Context) []byte {
+func (fm *filterManager) outputFilter(data []byte, context Context) []byte {
 	n := len(fm.filters)
 	for i := 0; i < n; i++ {
 		data = fm.filters[i].OutputFilter(data, context)
