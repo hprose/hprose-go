e03b40ddf127731a045f4409943638e1309b6a6b
Ma Bingyao <mabingyao@gmail.com>
2016-09-15 00:08:23 +0800

    Update examples

--- examples/fasthttp_hello_server/fasthttp_hello_server.go
+++ examples/fasthttp_hello_server/fasthttp_hello_server.go
@@ -1,46 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"reflect"
-
-	"github.com/hprose/hprose-golang/io"
 	"github.com/hprose/hprose-golang/rpc"
 	"github.com/valyala/fasthttp"
 )
 
-func hello(name string, context *rpc.FastHTTPContext) string {
-	return "Hello " + name + "!  -  " + context.RequestCtx.RemoteAddr().String()
-}
-
-type A struct {
-	S string `json:"str"`
-}
-
-func getEmptySlice() interface{} {
-	s := make([]A, 100)
-	return s
-}
-
-type ServerEvent struct{}
-
-func (e *ServerEvent) OnBeforeInvoke(name string, args []reflect.Value, byref bool, context rpc.Context) {
-	fmt.Println("Before OK")
-}
-
-func (e *ServerEvent) OnAfterInvoke(name string, args []reflect.Value, byref bool, result []reflect.Value, context rpc.Context) {
-	fmt.Println("After OK")
-}
-func (e *ServerEvent) OnSendError(err error, context rpc.Context) {
-	fmt.Println(err)
+func hello(name string) string {
+	return "Hello " + name + "!"
 }
 
 func main() {
-	io.Register(reflect.TypeOf(A{}), "A", "json")
 	service := rpc.NewFastHTTPService()
-	service.Event = &ServerEvent{}
-	service.Debug = true
 	service.AddFunction("hello", hello, rpc.Options{})
-	service.AddFunction("getEmptySlice", getEmptySlice, rpc.Options{})
 	fasthttp.ListenAndServe(":8080", service.ServeFastHTTP)
 }
