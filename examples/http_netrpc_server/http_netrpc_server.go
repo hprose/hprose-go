972a69c4a0da99905d15e914fb1dd080b5b99ac9
Ma Bingyao <mabingyao@gmail.com>
2016-09-16 18:43:38 +0800

    Fixed for golint

--- examples/http_netrpc_server/http_netrpc_server.go
+++ examples/http_netrpc_server/http_netrpc_server.go
@@ -7,21 +7,26 @@ import (
 	"github.com/hprose/hprose-golang/rpc"
 )
 
+// Args ...
 type Args struct {
 	A, B int
 }
 
+// Quotient ...
 type Quotient struct {
 	Quo, Rem int
 }
 
+// Arith ...
 type Arith int
 
+// Multiply ...
 func (t *Arith) Multiply(args *Args, reply *int) error {
 	*reply = args.A * args.B
 	return nil
 }
 
+// Divide ...
 func (t *Arith) Divide(args *Args, quo *Quotient) error {
 	if args.B == 0 {
 		return errors.New("divide by zero")
