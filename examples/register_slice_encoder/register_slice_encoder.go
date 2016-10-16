01e6f83f684b570df755984a30e539774ea0bf46
Ma Bingyao <mabingyao@gmail.com>
2016-10-17 07:44:09 +0800

    Update examples

--- examples/register_slice_encoder/register_slice_encoder.go
+++ examples/register_slice_encoder/register_slice_encoder.go
@@ -23,28 +23,22 @@ func mySliceEncoder(w *io.Writer, v interface{}) {
 	}
 }
 
-func main() {
-	slice := make([]map[string]interface{}, 10)
-	for i := 0; i < 10; i++ {
-		slice[i] = make(map[string]interface{})
-		slice[i]["id"] = i
-	}
-
-	fmt.Printf("%s\r\n", io.Marshal(slice))
+func test(slice []map[string]interface{}) {
 	start := time.Now()
 	for i := 0; i < 500000; i++ {
 		io.Marshal(slice)
 	}
 	stop := time.Now()
 	fmt.Println((stop.UnixNano() - start.UnixNano()) / 1000000)
+}
 
-	io.RegisterSliceEncoder(([]map[string]interface{})(nil), mySliceEncoder)
-
-	fmt.Printf("%s\r\n", io.Marshal(slice))
-	start = time.Now()
-	for i := 0; i < 500000; i++ {
-		io.Marshal(slice)
+func main() {
+	slice := make([]map[string]interface{}, 10)
+	for i := 0; i < 10; i++ {
+		slice[i] = make(map[string]interface{})
+		slice[i]["id"] = i
 	}
-	stop = time.Now()
-	fmt.Println((stop.UnixNano() - start.UnixNano()) / 1000000)
+	test(slice)
+	io.RegisterSliceEncoder(([]map[string]interface{})(nil), mySliceEncoder)
+	test(slice)
 }
